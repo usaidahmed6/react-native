abaca6fc22bfb78238d570be6d336a06b290fef3
usaidahmed6 <96471076+usaidahmed6@users.noreply.github.com>
2023-02-22 11:02:58 -0800

    Add files via upload

---
+++ stack navigation/config/StackNavigation.js
@@ -0,0 +1,14 @@
+// import { View, Text } from 'react-native'
+// import React from 'react'
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createNativeStackNavigator } from '@react-navigation/native-stack';
+// import Home from '../component/Home';
+
+// const StackNavigation = () => {
+//     const Stack = createNativeStackNavigator();
+//     return (
+        
+//     )
+// }
+
+// export default StackNavigation
